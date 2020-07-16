9ac935ff73f06315878fd913289e6bda3a149c2f
masoncusack <1masoncusack@gmail.com>
2020-07-16 19:37:07 +0100

    separate scripts, refactoring

--- upload_video.py
+++ upload_video.py
@@ -1,39 +1,34 @@
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-from dotenv import load_dotenv
 import os
+import requests
+
+from dotenv import load_dotenv
 
 load_dotenv()
 
+url = """
+      https://api.videoindexer.ai/{location}/Accounts/{account_id}/Videos?name={video_name}&privacy={privacy}&priority={priority}&description&language={language}&fileName={video_filename}&sendSuccessEmail={success_email}&accessToken={auth_token}
+      """.format(
+        location = os.getenv('LOCATION'),
+        account_id = os.getenv('ACCOUNT_ID'),
+        video_name = os.getenv('UPLOADED_VIDEO_NAME'),
+        privacy = os.getenv('PRIVACY', default='private'),
+        priority = os.getenv('PRIORITY', default='High'),
+        language = os.getenv('VIDEO_LANGUAGE', default='English'),
+        success_email = os.getenv('SUCCESS_EMAIL', default='True'),
+        video_filename = os.getenv('VIDEO_FILENAME'),
+        auth_token = os.getenv('AUTH_TOKEN')
+      )
+
+print("\nRequest url: {}".format(url))
+
+files = {
+  'file': ('', open(os.getenv('VIDEO_FILEPATH'),'rb'), 'multipart/form-data')
+}
 headers = {
-    # Request headers
-    'Content-Type': 'multipart/form-data',
-    'Ocp-Apim-Subscription-Key': os.getenv('APIM_SUBSCRIPTION_KEY'),
+  #'Content-Type': 'multipart/form-data',
+  'Ocp-Apim-Subscription-Key': os.getenv('APIM_SUBSCRIPTION_KEY')
 }
 
-# See API reference for below parameter and additional options:
+response = requests.post(url, headers=headers, files=files)
 
-params = urllib.parse.urlencode({
-    # Request parameters
-    'privacy': os.getenv('PRIVACY', default='Private'),
-    'priority': os.getenv('PRIORITY', default='High'),
-    'language': os.getenv('VIDEO_LANGUAGE', default='English'),
-    'fileName': os.getenv('VIDEO_FILENAME'),
-    'sendSuccessEmail': os.getenv('SEND_SUCCESS_EMAIL', default='True'),
-    'accessToken': os.getenv('AUTH_TOKEN'),
-})
-
-location = os.getenv('LOCATION', default='westus2')
-account_id = os.getenv('ACCOUNT_ID')
-video_name = os.getenv('VIDEO_NAME', default='New%20Video')
-
-# TODO: pull in file data from os as multipart form body
-
-try:
-    conn = http.client.HTTPSConnection('api.videoindexer.ai')
-    conn.request("POST", "/" + location + "/Accounts/" + account_id + "/Videos?name=" + video_name + "&%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+print(response.text.encode('utf8'))
